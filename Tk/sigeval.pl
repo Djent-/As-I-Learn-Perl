d7eedb0aed8730eafa26eb63913320603c63718d
PH <djent@cock.li>
2015-09-26 13:04:31 -0400

    Continuing tutorial

--- Tk/sigeval.pl
+++ Tk/sigeval.pl
@@ -53,4 +53,42 @@ $mw->configure(-menu => $menu_bar);
 # The ListBox is our filename container.
 my($ListBox) = $left_frame->Scrolled('ListBox', -height => '0', -width => '0', -scrollbars => 'e');
 
+# The default key bindings for text boxes are emacs-ish
+
+# The 'o' in 'osoe' means optionally, so the scrollbars will only appear on the
+# south ('s') and east ('e') sides when needed.
+
+my($InputText) = $right_frame->Scrolled('TextUndo', -height => '1', -width => '1', -scrollbars => 'osoe');
+
+# We use the Text widget here because we do not need to edit anything in the widget. We could
+# have used a read-only text widget here too (ROText).
+
+my($OutputText) = $right_frame->Scrolled('Text', -height => '1', -width => '1', -scrollbars => 'osoe');
+
+# Load filenames into the listbox:
+
+opendir DIR, ".";
+$ListBox->insert('end', grep { -f $_ } readdir DIR);
+close DIR;
+
+# Binding subs to events:
+
+# Every widget either creates or reacts to events.
+
+# Callbacks are subs that are used to react to events. Callbacks are bound to widgets.
+
+# You can use anonymous subs or reference the callback sub when creating callbacks.
+
+# Left mouse button loads file and eval's if it has a .pl suffix.
+
+$ListBox->bind( '<ButtonRelease-1>', [/&OnLoad] );
+
+# Ctrl-L: eval text widget contents
+
+$mw->bind( 'Tk::TextUndo', '<Control-key-l>', sub { OnEval(); } );
+
+# Ctrl-O: load a text file into the widget
+
+$mw->bind( 'Tk::TextUndo', '<Control-key-o>', sub { OnFileOpen(); } );
+
 
